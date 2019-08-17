6eb0d0380aa1dd18f343036eefc8413738fef497
Ryan Ansberry <ansman92@gmail.com>
2019-08-16 17:00:32 -0700

    Setting up route

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const helmet = require('helmet');
 
+const ProjectRouter = require('./routes/project-router');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/api/projects', ProjectRouter);
 
 
 server.get('/', (req, res) => {
